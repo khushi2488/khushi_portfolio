76e725b8d88650ffba4bc687056c9ba0d785696c
Khushi Patel <khushiiip2004@gmail.com>
2025-07-25 21:48:43 +0530

    Fixed dependency conflicts and rebuilt site

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Tilt } from 'react-tilt';
-
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,26 +7,26 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
+// ServiceCard Component
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    tiltMaxAngleX={25}
+    tiltMaxAngleY={25}
+    perspective={1000}
+    transitionSpeed={450}
+    scale={1.05}
+    className='xs:w-[250px] w-full'
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
         <img
           src={icon}
-          alt='web-development'
+          alt='service-icon'
           className='w-16 h-16 object-contain'
         />
-
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
         </h3>
@@ -36,6 +35,7 @@ const ServiceCard = ({ index, title, icon }) => (
   </Tilt>
 );
 
+// About Component
 const About = () => {
   return (
     <>
@@ -48,9 +48,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-      Hi, I’m Khushi — a Python developer with a growing passion for Artificial Intelligence and Machine Learning. I'm deeply curious about how machines learn and solve real-world problems.
-I love diving into Data Structures and Algorithms, constantly improving my problem-solving skills through hands-on coding. Whether it’s optimizing logic or building smart solutions, I’m always up for the challenge.
-Let’s turn ideas into intelligent systems!
+        Hi, I’m Khushi — a Python developer with a growing passion for Artificial Intelligence and Machine Learning.
+        I'm deeply curious about how machines learn and solve real-world problems.
+        I love diving into Data Structures and Algorithms, constantly improving my problem-solving skills through hands-on coding.
+        Whether it’s optimizing logic or building smart solutions, I’m always up for the challenge.
+        Let’s turn ideas into intelligent systems!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
